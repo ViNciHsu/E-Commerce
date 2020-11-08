20c3bcce6ce6eed14002de80ba3aaa0d26a2eb90
ViNciHsu <ViNciHsu>
2020-11-09 00:07:53 +0800

    test controller

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -11,7 +11,8 @@ class SocialiteController extends Controller
 {
     public function googleSignInProcess()
     {
-        return Socialite::driver('google')->redirect();
+        return "test";
+//        return Socialite::driver('google')->redirect();
 //            ->with(
 //                ['client_id' => '453343820007-7ve95p6p0ivglhn4atav31kop17vnp6j.apps.googleusercontent.com'],
 //                ['client_secret' => 'qpQKmqk828Q4qA8naaMUqfmS'],
