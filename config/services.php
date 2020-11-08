d12fe2acf3ada057e473ed8b045f8b6304c8cce3
ViNciHsu <ViNciHsu>
2020-11-08 22:04:52 +0800

    modify services for GOOGLE_REDIRECT

--- config/services.php
+++ config/services.php
@@ -41,6 +41,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),         // Your Google Client ID
         'client_secret' => env('GOOGLE_CLIENT_SECRET'), // Your Google Client Secret
-        'redirect' => 'http://localhost:8000/google-sign-in-callback',
+        'redirect' => env('GOOGLE_REDIRECT'),
     ],
 ];
