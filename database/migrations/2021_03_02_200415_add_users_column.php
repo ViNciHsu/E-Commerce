58bb11873ada122f7b9fd0a0f9939f57a4cd6460
ViNciHsu <ViNciHsu>
2021-03-02 20:09:49 +0800

    add users column

---
+++ database/migrations/2021_03_02_200415_add_users_column.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUsersColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('user_level')
+                ->default(0);
+            $table->integer('add')
+                ->default(0);
+            $table->integer('edit')
+                ->default(0);
+            $table->integer('delete')
+                ->default(0);
+            $table->string('address_county');
+            $table->string('address_city');
+            $table->string('address_zip');
+            $table->string('address_street');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('user_level');
+            $table->dropColumn('add');
+            $table->dropColumn('edit');
+            $table->dropColumn('delete');
+            $table->dropColumn('address_county');
+            $table->dropColumn('address_city');
+            $table->dropColumn('address_zip');
+            $table->dropColumn('address_street');
+        });
+    }
+}
